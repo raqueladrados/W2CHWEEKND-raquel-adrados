e96e73d61da24a06cbe7fb64d44c2752bf0c2add
Raquel Adrados <radrados00@gmail.com>
2023-05-06 13:41:29 +0200

    Add examples tests

--- app/main.test.js
+++ app/main.test.js
@@ -1,11 +1,31 @@
 import lenk from "./main.js";
 describe("Given a length function", () => {
   describe("When it receives an array", () => {
-    test("Then it should return true", () => {
+    test("Then it should return 3", () => {
       const array = ["nube", "sol", "estrellas"];
-      const expectedResult = true;
+      const expectedResult = 3;
 
       const result = lenk(array);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+  describe("When it receives an array", () => {
+    test("Then it should return 5", () => {
+      const array = ["tren", "rail", "estación", "parada", "línea"];
+      const expectedResult = 5;
+
+      const result = lenk(array);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+  describe("When it receives an string", () => {
+    test("Then it should return 7", () => {
+      const string = "bacalao";
+      const expectedResult = 7;
+
+      const result = lenk(string);
 
       expect(result).toBe(expectedResult);
     });
